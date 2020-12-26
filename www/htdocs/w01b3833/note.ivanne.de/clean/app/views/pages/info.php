10f01db6f23a2773c20fd7611f178c52599d52b0
Ivanne <Ivanne@DESKTOP-K435EVT>
2020-12-26 22:32:32 +0100

    Add YT link

--- www/htdocs/w01b3833/note.ivanne.de/clean/app/views/pages/info.php
+++ www/htdocs/w01b3833/note.ivanne.de/clean/app/views/pages/info.php
@@ -9,12 +9,17 @@ include_once APPROOT . '/views/includes/head.php';
     <div class="wrapper center">
         <h1>Info</h1>
 
-        <p>Angemeldete Besucher können auf dieser Website persönliche Notizen und Termine verwalten.</p>
+        <p>Angemeldete Besucher können auf dieser Website persönliche Notizen bzw. Termine verwalten.</p>
 
         <p class="linking">
             Diese Webapplikation ist ein Open-Source-Projekt.
             <a target="_blank" href="https://github.com/ivannit/clean/tree/master/www/htdocs/w01b3833/note.ivanne.de/clean">
-                Hier Quellcode ansehen</a>
+                Quellcode ansehen</a>
+        </p>
+        
+        <p class="linking">
+            <a target="_blank" href="https://www.youtube.com/channel/UC27jKTT3SQWNvEevoJ6Nbnw">
+                Youtube-Kanal ansehen</a>
         </p>
 
     </div>
